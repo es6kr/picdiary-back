42814d6b26984b61595e07656823b0ac7866f094
Jinah <monsatfri@gmail.com>
2024-02-15 06:09:18 +0900

    build: initial commit

---
+++ src/main/java/picdiary/PicdiaryBackApplication.java
@@ -0,0 +1,13 @@
+package picdiary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PicdiaryBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PicdiaryBackApplication.class, args);
+	}
+
+}
