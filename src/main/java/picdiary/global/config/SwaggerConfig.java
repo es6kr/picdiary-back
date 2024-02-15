1a66d9b3d24eaedefed1125e65401b678a8fbb04
Hayoung <drumrobot43@gmail.com>
2024-02-15 23:50:19 +0900

    feat: dev/배포 링크를 나누어 Swagger 설정

--- src/main/java/picdiary/global/config/SwaggerConfig.java
+++ src/main/java/picdiary/global/config/SwaggerConfig.java
@@ -6,17 +6,20 @@ import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
 import io.swagger.v3.oas.models.security.SecurityScheme;
 import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class SwaggerConfig {
+    @Value("${swagger.url}") private String url;
+
     @Bean
     public OpenAPI customOpenAPI() {
-        return new OpenAPI().addServersItem(new Server().url("https://diary.es6.kr/"))
+        return new OpenAPI().addServersItem(new Server().url(url))
             .components(new Components().addSecuritySchemes("basicScheme",
                 new SecurityScheme().type(SecurityScheme.Type.HTTP).scheme("basic")))
             .info(new Info().title("Picdiary API").version("V1")
-                .license(new License().name("Apache 2.0").url("<https://diary.es6.kr>")));
+                .license(new License().name("Apache 2.0").url(url)));
     }
 }
